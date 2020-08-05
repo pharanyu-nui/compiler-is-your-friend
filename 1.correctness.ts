dba86e077bfc889540ac5aa3f28239992e86798b
pharanyu <pharanyu.c@codium.co>
2020-08-05 14:04:20 +0700

    Q1:1)defind type to string to input of func 2)how to defind type

--- 1.correctness.ts
+++ 1.correctness.ts
@@ -12,3 +12,7 @@ Questions:
 1) How to ensure that the input is a string at compile time?
 2) What did you learn from this exercise?
 */
+
+function scriptAt(s: string): number {
+    return s.indexOf('Script');
+}
